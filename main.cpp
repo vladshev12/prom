d8fb68989b2fc6d5cdd0aa66d9ed6a953f49aba0
Vlassh <vladhsheva28@gmail.com>
2026-05-19 15:51:56 +0300

    feat: add calculateCircleArea function

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
-
+const double PI = 3.14159;
 double calculateRectangleArea(double width, double height) {
     return width * height;
 }
-
+double calculateCircleArea(double radius) {
+    return PI * radius * radius;
+}
 int main() {
     std::cout << "=== Геометрический калькулятор ===" << std::endl;
-    
     double w = 5.0, h = 10.0;
     std::cout << "Площадь прямоугольника (" << w << "x" << h << "): " 
               << calculateRectangleArea(w, h) << std::endl;
+    double r = 7.0;
+    std::cout << "Площадь круга (r=" << r << "): " 
+              << calculateCircleArea(r) << std::endl;
               
     return 0;
 }
